2600b8f9d09acb205170fab4cf7fd9adcb055adf
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2021-10-31 20:22:56 +0300

    Add functions to convert fraction to number, change date format, and get total number of goals

--- upload_file.js
+++ upload_file.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 const timezone = 'Africa/Nairobi';
 moment.locale('en');
 
-fs.readFile('./inverse.json', 'utf8', (err, data) => {
+fs.readFile('./2019-01-01.json', 'utf8', (err, data) => {
     if (err) {
         console.error(err)
         return
@@ -20,21 +20,26 @@ fs.readFile('./inverse.json', 'utf8', (err, data) => {
             object.competition = item.tournament.category.name.concat(" - ", item.tournament.name);
             object.home_team = getValueWithKey(item.homeTeam, "name");
             object.away_team = getValueWithKey(item.awayTeam, "name");
-            object.half_time = getValueWithKey(item.homeScore, "period1") + "-" + getValueWithKey(item.awayScore, "period1");
+            object.halftime_score = getValueWithKey(item.homeScore, "period1") + "-" + getValueWithKey(item.awayScore, "period1");
             object.second_half = getValueWithKey(item.homeScore, "period2") + "-" + getValueWithKey(item.awayScore, "period2");
             object.full_time = getValueWithKey(item.homeScore, "normaltime") + "-" + getValueWithKey(item.awayScore, "normaltime");
-            object.half_time_result = getHalftimeWinner(getValueWithKey(item.homeScore, "period1"), getValueWithKey(item.awayScore, "period1"));
+            object.home_score = getValueWithKey(item.homeScore, "normaltime");
+            object.away_score = getValueWithKey(item.awayScore, "normaltime");
+            object.both_total = getTotalGoals(getValueWithKey(item.homeScore, "normaltime"), getValueWithKey(item.awayScore, "normaltime"));
+            object.halftime_result = getHalftimeWinner(getValueWithKey(item.homeScore, "period1"), getValueWithKey(item.awayScore, "period1"));
+            object.home_record = JSON.stringify(getValueWithKey(item, "homeScore"));
+            object.away_record = JSON.stringify(getValueWithKey(item, "awayScore"));
             object.result = getValueWithKey(item, "winnerCode") == 3 ? "X" : getValueWithKey(item, "winnerCode");
             if (odds_2[object.match_id] !== undefined) {
                 object.home_change = odds_2[object.match_id].choices[0].change;
                 object.draw_change = odds_2[object.match_id].choices[1].change;
                 object.away_change = odds_2[object.match_id].choices[2].change;
-                object.home_odd = odds_2[object.match_id].choices[0].fractionalValue;
-                object.draw_odd = odds_2[object.match_id].choices[1].fractionalValue;
-                object.away_odd = odds_2[object.match_id].choices[2].fractionalValue;
+                object.home_odd = convertFractionToDecimal(odds_2[object.match_id].choices[0].fractionalValue);
+                object.draw_odd = convertFractionToDecimal(odds_2[object.match_id].choices[1].fractionalValue);
+                object.away_odd = convertFractionToDecimal(odds_2[object.match_id].choices[2].fractionalValue);
             }
             
-            object.event_date = moment.unix(item.startTimestamp).tz(timezone).format("L");
+            object.event_date = moment.unix(item.startTimestamp).tz(timezone).format("YYYY-MM-DD HH:mm:ss");
             count += 1;
             console.log(object);
         });
@@ -42,7 +47,6 @@ fs.readFile('./inverse.json', 'utf8', (err, data) => {
         console.log(err);
     }
 });
-
 
 var getValueWithKey = function(object, key) {
     if (_.has(object, key)) {
@@ -59,4 +63,14 @@ var getHalftimeWinner = function (home_score, away_score) {
     if (home_score_int < away_score_int) return 2;
 
     return "-";
+}
+
+var getTotalGoals = function (home_score, away_score) {
+    return parseInt(home_score) + parseInt(away_score);
+}
+
+var convertFractionToDecimal = function(number_to_convert) {
+    let temp_numb = number_to_convert.split("/");
+
+    return (Math.round((((temp_numb[0]/temp_numb[1])/1) + Number.EPSILON) * 100) / 100) + 1;
 }
