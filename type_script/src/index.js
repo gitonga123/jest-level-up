07c07b1b4fedf3191d01502b2587f773192a76bd
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-02-21 19:59:42 +0300

    Defining variables in type script

---
+++ type_script/src/index.js
@@ -0,0 +1,36 @@
+var id = 6;
+var company = "OTB AFRICA";
+var isPublished = true;
+var x = "Hello";
+var age = 30;
+var ids = [5, 7, 6, 35];
+var arr = ["Daniel", "Mutwiri"];
+// Tuple - you can define  multiple types
+var person = [1, "Daniel", true];
+// tuple array
+var employee;
+employee = [
+    [1, "Brad"],
+    [2, "John"],
+    [3, "Jill"],
+];
+// union - a variable can hold multiple types
+var pid;
+// Enum - enumerated types
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var user = {
+    id: 1,
+    name: "James"
+};
+console.log(Direction1.Up, Direction1.Down);
+console.log(person);
+// Type Assertion - telling a compiler we are treating an entity as a different type
+var cid = 1;
+var customerId = cid;
+var customerTrue = true;
--- type_script/src/index.ts
+++ type_script/src/index.ts
@@ -1,5 +1,55 @@
-let id: number;
+let id: number = 6;
+let company: string = "OTB AFRICA";
+let isPublished: boolean = true;
+let x: any = "Hello";
 
-id = 5;
+let age: number = 30;
 
-console.log(5);
+let ids: number[] = [5, 7, 6, 35];
+
+let arr: any[] = ["Daniel", "Mutwiri"];
+
+// Tuple - you can define  multiple types
+let person: [number, string, boolean] = [1, "Daniel", true];
+
+// tuple array
+
+let employee: [number, string][];
+
+employee = [
+  [1, "Brad"],
+  [2, "John"],
+  [3, "Jill"],
+];
+
+// union - a variable can hold multiple types
+
+let pid: string | number;
+
+// Enum - enumerated types
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+// Objects
+type User = {
+  id: Number;
+  name: String;
+};
+
+const user: User = {
+  id: 1,
+  name: "James",
+};
+
+console.log(Direction1.Up, Direction1.Down);
+console.log(person);
+
+// Type Assertion - telling a compiler we are treating an entity as a different type
+let cid: any = 1
+let customerId = <number>cid;
+let customerTrue = <boolean>true;
+
